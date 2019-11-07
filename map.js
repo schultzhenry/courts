bbfca3213f24d7305ac44ddc785d308e5d3c9a2c
schultzhenry <natalie.schultz-henry@yale.edu>
2019-11-06 21:33:34 -0500

    add basic us map

--- map.js
+++ map.js
@@ -38,13 +38,13 @@ var rectWidth = 22;
 var rectHeight = 14;
 
 // Guam
-svg.append('rect').attrs({class: , id: 91, x: 240, y: 330, width: rectWidth, height: rectHeight});
+svg.append('rect').attrs({class: "map-item", id: 91, x: 240, y: 330, width: rectWidth, height: rectHeight});
 
 // Northern Mariana Islands
-svg.append('rect').attrs({class: , id: 92, x: 240, y: 300, width: rectWidth, height: rectHeight});
+svg.append('rect').attrs({class: "map-item", id: 92, x: 240, y: 300, width: rectWidth, height: rectHeight});
 
 // Puerto Rico
-svg.append('rect').attrs({class: , id: 93, x: 510, y: 362, width: rectWidth, height: rectHeight});
+svg.append('rect').attrs({class: "map-item", id: 93, x: 510, y: 362, width: rectWidth, height: rectHeight});
 
 // Virgin Islands
-svg.append('rect').attrs({class: , id: 94, x: 550, y: 362, width: rectWidth, height: rectHeight});
+svg.append('rect').attrs({class: "map-item", id: 94, x: 550, y: 362, width: rectWidth, height: rectHeight});
