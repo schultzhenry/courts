949e191d3ef09482bd25eeb14e5892ba1a5d9abd
schultzhenry <natalie.schultz-henry@yale.edu>
2019-11-07 14:34:59 -0500

    update map hovering styling/functionality

--- interaction.js
+++ interaction.js
@@ -1,3 +1,5 @@
+// HANDLE INTERACTION WITH MAP AND COURT VIS ELEMENTS
+
 var rightPanelOpen = false;
 
 function toggleRightPanel() {
@@ -45,13 +47,86 @@ $('.court').click(function() {
   $('#right-header > h1').text(this.id.replace('-', ' '));
 });
 
-// $('path.district').hover(function() {
-//   console.log("hey");
-//   console.log($(this).attr('id'));
-// });
 
+
+
+// Handle hovering over districts on map
 $(document).on('mouseenter','.district', function (event) {
+
+  // Do nothing if hovering over territory label
+  if ($(this).get(0).tagName == 'text') {
+    console.log('text');
+    return;
+  }
+  else if ($(this).get(0).tagName == 'path') {
+    // Grey out other districts
+    $('.district').addClass('inactive-hover');
+
+    // Add copy of district on top of svg and highlight
+    var temp = $(this).clone().appendTo($(this).parent())
+      .removeClass('inactive-hover')
+      .addClass('active-hover');
+
+    // Highlight corresponding courts
+    $('.'+String($( this ).attr('id'))).addClass('active-hover');
+
+    return;
+  }
+  else if ($(this).get(0).tagName == 'rect') {
+
+    // Grey out other districts
+    $('.district').addClass('inactive-hover');
+
+    // Highlight district boundary and text
+    $('#'+String($( this ).attr('id'))).removeClass('inactive-hover').addClass('active-hover');
+    $('text#'+String($( this ).attr('id'))).removeClass('inactive-hover');
+
+    // Highlight corresponding courts
+    $('.'+String($( this ).attr('id'))).addClass('active-hover');
+
+  }
+
+}).on('mouseleave','.district',  function() {
+
+  // Do nothing if hovering over territory label
+  if ($(this).get(0).tagName == 'text') {
+    console.log('text');
+    return;
+  }
+  else if ($(this).get(0).tagName == 'path') {
+    // Remove active and inactive classes from all districts
+    $('.district').removeClass('active-hover').removeClass('inactive-hover');
+
+    // Remove active class from all courts
+    $('.court').removeClass('active-hover');
+
+    // Remove original district from bottom of svg
+    $( this ).remove();
+
+    return;
+  } else if ($(this).get(0).tagName == 'rect') {
+
+    // Remove active and inactive classes from all districts
+    $('.district').removeClass('active-hover').removeClass('inactive-hover');
+
+    // Remove active class from all courts
+    $('.court').removeClass('active-hover');
+
+  }
+
+  // // If a territory, remove label from bottome of svg
+  // if ($(this).get(0).tagName == 'rect') {
+  //   $('text.old-label').remove().delay( 800 );
+  // }
+});
+
+
+
+
+$(document).on('mouseenter','.court', function (event) {
+  var classes = $( this ).attr('class').replace('court ', '').split(' ');
+  classes.forEach(function(i) { console.log(i); });
   $('.'+String($( this ).attr('id'))).addClass('active-hover');
-}).on('mouseleave','.district',  function(){
+}).on('mouseleave','.court',  function(){
   $('.'+String($( this ).attr('id'))).removeClass('active-hover');
 });
--- map.js
+++ map.js
@@ -1,54 +1,68 @@
-// load map
-var mapw = 520;
-var maph = 320;
+// DRAW MAP OF THE US WITH COUNTIES AND JUDICIAL DISTRICTS
 
-var svg = d3.select("#left-vis").append('svg')
-  .attr('id', 'map-vis')
-  .attr('width', mapw)
-  .attr('height', maph)
-  .append('g')
-  .attr('class', 'map')
-  .attr("transform", "translate("+String((-mapw/2)+20)+","+String((maph/2)-230)+")");
+function drawMap(mapw, maph) {
 
-var path = d3.geoPath();
-// var color = d3.scaleOrdinal("schemeCategory20b");
+  // Append svg
+  var svg = d3.select("#left-vis").append('svg')
+    .attrs({
+      id: 'map-vis',
+      width: 520,
+      height: 320
+    })
+    .append('g')
+    .attrs({
+      class: 'map',
+      transform: "translate("+String((-mapw/2)+20)+","+String((maph/2)-230)+")"
+    });
 
-// Draw county boundaries
-d3.json("us_jud.json", function(error, us) {
-  if (error) throw error;
+  // Draw county and district boundaries
+  var path = d3.geoPath();
+  d3.json("us.json", function(error, us) {
+    if (error) throw error;
 
-  var counties = svg.append("g").attr("id", "counties").selectAll("path")
-  .data(topojson.feature(us, us.objects.counties).features)
-  .enter().append("path")
-  .attr("class", "county")
-  .attr("d", d3.geoPath().projection(d3.geoAlbersUsa().scale(550)));
+    var counties = svg.append("g").attr("id", "counties").selectAll("path")
+      .data(topojson.feature(us, us.objects.counties).features)
+      .enter().append("path")
+      .attrs({
+        class: "county",
+        d: d3.geoPath().projection(d3.geoAlbersUsa().scale(550))
+      });
 
-  var districts = svg.append("g").attr("id", "districts").selectAll("path.district")
-  .data(topojson.feature(us, us.objects.districts).features)
-  .enter().append("path")
-  .attr("class", "district")
-  .attr("d", d3.geoPath().projection(d3.geoAlbersUsa().scale(550)));
+    var districts = svg.append("g").attr("id", "districts").selectAll("path.district")
+      .data(topojson.feature(us, us.objects.districts).features)
+      .enter().append("path")
+      .attrs({
+        class: "district",
+        d: d3.geoPath().projection(d3.geoAlbersUsa().scale(550))
+      });
 
-  districts.each(function(d) {
-    districts.attr("id", function(d) { return d.properties.jdcode;});
+    districts.each(function(d) {
+      districts.attr("id", function(d) {
+        return 'c'+String(d.properties.jdcode);
+      });
+    });
   });
-});
 
-var rectWidth = 22;
-var rectHeight = 14;
+  // Add US territories not represented in map
+  var rectWidth = 22;
+  var rectHeight = 14;
 
-// Guam
-svg.append('rect').attrs({class: "district", id: 91, x: 255, y: 310, width: rectWidth, height: rectHeight})
-svg.append('text').attrs({x: (255+(rectWidth/2)), y: (310+rectHeight-3)}).text("GU").style("text-anchor", "middle");
+  // Guam
+  svg.append('rect').attrs({class: "district", id: 'c91', x: 255, y: 310, width: rectWidth, height: rectHeight})
+  svg.append('text').attrs({class: "district", id: 'c91', x: (255+(rectWidth/2)), y: (310+rectHeight-3)}).text("GU").style("text-anchor", "middle");
 
-// Northern Mariana Islands
-svg.append('rect').attrs({class: "district", id: 92, x: 255, y: 280, width: rectWidth, height: rectHeight});
-svg.append('text').attrs({x: (255+(rectWidth/2)), y: (280+rectHeight-3)}).text("MP").style("text-anchor", "middle");
+  // Northern Mariana Islands
+  svg.append('rect').attrs({class: "district", id: 'c92', x: 255, y: 280, width: rectWidth, height: rectHeight});
+  svg.append('text').attrs({class: "district", id: 'c92', x: (255+(rectWidth/2)), y: (280+rectHeight-3)}).text("MP").style("text-anchor", "middle");
 
-// Puerto Rico
-svg.append('rect').attrs({class: "district", id: 93, x: 510, y: 362, width: rectWidth, height: rectHeight});
-svg.append('text').attrs({x: (510+(rectWidth/2)), y: (362+rectHeight-3)}).text("PR").style("text-anchor", "middle");
+  // Puerto Rico
+  svg.append('rect').attrs({class: "district", id: 'c93', x: 510, y: 362, width: rectWidth, height: rectHeight});
+  svg.append('text').attrs({class: "district", id: 'c93', x: (510+(rectWidth/2)), y: (362+rectHeight-3)}).text("PR").style("text-anchor", "middle");
 
-// Virgin Islands
-svg.append('rect').attrs({class: "district", id: 94, x: 550, y: 362, width: rectWidth, height: rectHeight});
-svg.append('text').attrs({x: (550+(rectWidth/2)), y: (362+rectHeight-3)}).text("VI").style("text-anchor", "middle");
+  // Virgin Islands
+  svg.append('rect').attrs({class: "district", id: 'c94', x: 550, y: 362, width: rectWidth, height: rectHeight});
+  svg.append('text').attrs({class: "district", id: 'c94', x: (550+(rectWidth/2)), y: (362+rectHeight-3)}).text("VI").style("text-anchor", "middle");
+
+}
+
+drawMap(520, 320);
