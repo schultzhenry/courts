46b1eff3d7d834075bc34355c48673f13034fcae
schultzhenry <natalie.schultz-henry@yale.edu>
2019-12-13 10:51:29 -0500

    update helpers

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -5,11 +5,14 @@
 var toggleAllJudges = false;
 var detailPanelOpen = false;
 var mapOpen = false;
+var activeCourt = '';
 
 // VIEW CONSTANTS
-var activeCourt = '';
-var presidentIdList = '#Donald-J-Trump, #Barack-Obama, #George-W-Bush, #William-J-Clinton, #George-HW-Bush, #Ronald-Reagan';
-var column_names = [ 'Judge', 'Appointing President', 'Party', 'Commission', 'Years on Court', 'ABA Rating' ]
+const column_names = [ 'Judge', 'Appointing President', 'Party', 'Commission', 'Years on Court', 'ABA Rating' ]
+const presList = [ 'Donald J. Trump', 'Barack Obama', 'George W. Bush', 'William J. Clinton', 'George H.W. Bush', 'Ronald Reagan' ]
+const presClassList = Array.from(presList, e => '.'+f3(f(e)));
+const presIdList = Array.from(presList, e => '#'+f3(f(e)));
+const presidentIdList = presIdList.join(', ');
 
 // JUDGE COUNTS
 var activeJ = 0;
@@ -18,9 +21,10 @@ var demJ = 0;
 var repJ = 0;
 var presJ = Array(presidentIdList.length).fill(0);
 
-// FORMAT STRINGS
+// FORMAT STRING
 function f(s) { return s.replace(/ /g, '-'); }
 function f2(s) { return s.replace(/\-/g, ' '); }
+function f3(s) { return s.replace(/\./g, ''); }
 
 // TOGGLE OBJECT VISIBILITY
 function toggleVis(t,b) { if (b) { $(t).show(); } else { $(t).hide(); } }
