cb2e68b1d0154311358e8ad262915d403905db29
schultzhenry <natalie.schultz-henry@yale.edu>
2019-12-13 10:07:21 -0500

    cpar491 submission

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,3 +1,5 @@
+// HELPERS.JS: GLOBAL VARIABLES AND HELPER FUNCTIONS
+// FOR THE FEDERAL JUDICIARY INTERACTIVE VISUALIZATION
 
 // VIEW DEFAULTS
 var toggleAllJudges = false;
@@ -9,14 +11,16 @@ var activeCourt = '';
 var presidentIdList = '#Donald-J-Trump, #Barack-Obama, #George-W-Bush, #William-J-Clinton, #George-HW-Bush, #Ronald-Reagan';
 var column_names = [ 'Judge', 'Appointing President', 'Party', 'Commission', 'Years on Court', 'ABA Rating' ]
 
+// JUDGE COUNTS
+var activeJ = 0;
+var vacantJ = 0;
+var demJ = 0;
+var repJ = 0;
+var presJ = Array(presidentIdList.length).fill(0);
+
 // FORMAT STRINGS
 function f(s) { return s.replace(/ /g, '-'); }
-function f2(s) { return s.replace('-', ' '); }
+function f2(s) { return s.replace(/\-/g, ' '); }
 
 // TOGGLE OBJECT VISIBILITY
-function toggleVis(t,b) {
-  setTimeout(function() {
-    if (b) { $(t).show();
-    } else { $(t).hide(); }
-  }, 50);
-}
+function toggleVis(t,b) { if (b) { $(t).show(); } else { $(t).hide(); } }
